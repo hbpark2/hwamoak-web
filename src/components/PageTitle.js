cf25260722e057aa2a3c87b91481c84a0ac85669
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-09-14 17:45:01 +0900

    apollo

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -4,7 +4,7 @@ const { Helmet } = require('react-helmet-async');
 export const PageTitle = ({ title }) => {
   return (
     <Helmet>
-      <title> {title} | 화목 </title>
+      <title> 화목 | {title} </title>
     </Helmet>
   );
 };
