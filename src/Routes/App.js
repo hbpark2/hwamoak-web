4668ee679af2e95f491a016fd95df68b8685a9b2
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2022-03-24 14:47:39 +0900

    build

--- src/Routes/App.js
+++ src/Routes/App.js
@@ -1,14 +1,12 @@
 import { ApolloProvider, useReactiveVar } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme, GlobalStyles } from 'styles';
-
 import { HelmetProvider } from 'react-helmet-async';
 import { client, darkModeVar } from 'apollo';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import { StoreProvider } from 'Context/ContextStore';
-import Routers from './Routers';
 import Header from '../Components/Layout/Header';
+import Routers from './Routers';
 
 const App = () => {
   const darkMode = useReactiveVar(darkModeVar);
